9f40d06d9a7d32bfe3d7263554b068550ea09e7b
Camila Ribeiro de Moraes <camilaribeiro0308@gmail.com>
2021-06-25 20:54:57 -0300

    Lógica inicial estabelecida

---
+++ jogoDaVelha.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include<locale.h>
+
+char tabela [3][3];
+int chute,i,j;
+
+void iniciaVelha(){
+  printf("\n");
+  
+  for(int i = 0; i < 3; i++){
+    printf(" \t \n");
+    for(int j = 0; j < 3;j++){
+      printf("\t [ %c ] ", tabela[i][j]);
+      
+    } 
+  }
+  printf("\n");
+}
+void atualizaVelha(char i, char j){
+  char x,y;
+  tabela[i][j] = 'x';  
+    for(int i = 0; i < 3; i++){
+    printf(" \t \n");
+    for(int j = 0; j < 3;j++){
+      //printf("\t [ %c ] ", tabela[i][j]);
+      
+    } 
+  }
+  iniciaVelha();
+}
+void mostraJogada(){}
+int main(void) {
+
+  printf("\n");
+  printf(" \t #######################################################\n");
+  printf(" \t############### ESSE É O JOGO DA VELHA ################\n");
+  printf(" \t #######################################################\n");
+  iniciaVelha();
+  printf("\n \n ");
+
+    printf("Digite a linha a ser preenchida: ");
+    scanf("%d",&i);
+    printf("Digite a coluna a ser preenchida: ");
+    scanf("%d",&j);
+  atualizaVelha(i,j);
+  
+
+
+
+  return 0;
+}
