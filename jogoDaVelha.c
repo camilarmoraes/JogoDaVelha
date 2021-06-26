011df575302e818457c64e465293de05b42fa960
Camila Ribeiro de Moraes <camilaribeiro0308@gmail.com>
2021-06-25 21:15:11 -0300

    Multiplas jogadas

--- jogoDaVelha.c
+++ jogoDaVelha.c
@@ -22,7 +22,6 @@ void atualizaVelha(char i, char j){
     for(int i = 0; i < 3; i++){
     printf(" \t \n");
     for(int j = 0; j < 3;j++){
-      //printf("\t [ %c ] ", tabela[i][j]);
       
     } 
   }
@@ -37,12 +36,16 @@ int main(void) {
   printf(" \t #######################################################\n");
   iniciaVelha();
   printf("\n \n ");
-
+    do{
+    printf("\n");
     printf("Digite a linha a ser preenchida: ");
     scanf("%d",&i);
     printf("Digite a coluna a ser preenchida: ");
     scanf("%d",&j);
-  atualizaVelha(i,j);
+     atualizaVelha(i,j);
+    }while(i != 9 && j != 9);
+
+ 
   
 
 
